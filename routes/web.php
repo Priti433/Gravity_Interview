e24556b6ed5c0fcc9ac3044e2b6a2942bc6f33f9
Advyaan <advyaan@Advyaans-MacBook-Air.local>
2024-06-21 17:24:54 +0530

    optimozied the all three rules in single function

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Api\DiwaliSaleController;
 
-Route::post('/saleRule1', [DiwaliSaleController::class, 'saleRule1']);
+Route::post('/handleSaleRule', [DiwaliSaleController::class, 'handleSaleRule']);
 Route::post('/saleRule2', [DiwaliSaleController::class, 'saleRule2']);
 Route::post('/saleRule3', [DiwaliSaleController::class, 'saleRule3']);
